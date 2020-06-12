8aa656975caaa03ebe89c14a17219c407fcabbff
BigBlackWolf <stepan.rasputny@gmail.com>
2020-06-12 17:59:26 +0300

    fix router

--- dashboard/router.go
+++ dashboard/router.go
@@ -11,6 +11,7 @@ func CreateRouter(router *mux.Router) *mux.Router {
 	sub.HandleFunc("", handlers.IndexHandler).Methods("GET", "OPTIONS")
 	sub.HandleFunc("/", handlers.IndexHandler).Methods("GET", "OPTIONS")
 	sub.HandleFunc("", handlers.AddTaskHandler).Methods("POST", "OPTIONS")
+	sub.HandleFunc("/", handlers.AddTaskHandler).Methods("POST", "OPTIONS")
 	sub.HandleFunc("/{task_id:[0-9]+}", handlers.TaskHandler).Methods("GET", "OPTIONS")
 	sub.HandleFunc("/{task_id:[0-9]+}", handlers.DeleteTaskHandler).Methods("DELETE", "OPTIONS")
 	return router
